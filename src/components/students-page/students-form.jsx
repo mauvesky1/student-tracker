fca1cefcf99183426f5002b7f8a20d709fb31067
Brork <jamessamueljohnson@gmail.com>
2020-03-11 15:52:56 +0000

    test-commit

--- src/components/students-page/students-form.jsx
+++ src/components/students-page/students-form.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 
-const studentsForm = props => {
-  return (
-    <div>
-      <input></input>
-    </div>
-  );
-};
+class studentsForm extends React.Component {
+  state = {
+    input: ""
+  };
+
+  changeInput = event => {
+    console.log(event.target.value);
+    this.setState({ input: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            console.log(event);
+            this.props.filterStudents(this.props.studentData, this.state.input);
+          }}
+        >
+          <input
+            value={this.state.input}
+            onChange={this.changeInput}
+            type=""
+          ></input>
+          <button>Search By Block</button>
+        </form>
+        <button
+          onCLick={() => {
+            this.props.resetStudents;
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    );
+  }
+}
 
 export default studentsForm;
--- src/components/students-page/students-page.jsx
+++ src/components/students-page/students-page.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import axios from "axios";
 import StudentList from "./students-list";
+import StudentForm from "./students-form";
 
 class StudentPage extends React.Component {
-  state = { studentData: [], isLoading: true, query: "" };
+  state = { studentData: [], isLoading: true, query: "", visibleStudents: [] };
+
+  filterStudents = (studentData, block) => {
+    const filteredData = studentData.filter(student => {
+      if (student.currentBlock === block) {
+        return student;
+      }
+    });
+    this.setState({ visibleStudents: filteredData });
+  };
+
+  resetStudents = () => {
+    this.setState({ visibleStudents: this.state.studentData });
+  };
 
   componentDidMount = () => {
     axios
@@ -14,7 +28,16 @@ class StudentPage extends React.Component {
   };
 
   render() {
-    return <StudentList studentData={this.state.studentData} />;
+    return (
+      <div>
+        <StudentList studentData={this.state.visibleStudents} />
+        <StudentForm
+          filterStudents={this.filterStudents}
+          studentData={this.state.studentData}
+          resetStudents={this.resetStudents}
+        />
+      </div>
+    );
   }
 }
 
