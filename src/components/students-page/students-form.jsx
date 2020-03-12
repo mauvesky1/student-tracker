8c5830bd66695d8250981ebd644692c7393ce105
bob <rob.draper@live.com>
2020-03-12 14:52:18 +0000

    Added styling and more form functionality

--- src/components/students-page/students-form.jsx
+++ src/components/students-page/students-form.jsx
@@ -9,13 +9,11 @@ class studentsForm extends React.Component {
   };
 
   formatInput = () => {
-    const newQuery = `${this.state.primarySelectBox}=${this.state.secondarySelectBox}&`;
-    this.props.updateQuery(newQuery);
-    console.log(
-      "console log",
-      this.state.primarySelectBox,
-      this.state.secondarySelectBox
-    );
+    this.setState(function(currentState) {
+      currentState.finalQuery += `${currentState.primarySelectBox}=${currentState.secondarySelectBox}&`;
+
+      return { finalQuery: currentState.finalQuery };
+    });
   };
 
   changePrimarySelectBox = event => {
@@ -32,55 +30,93 @@ class studentsForm extends React.Component {
     return (
       <div>
         <form
+          className="inputGrid"
           onSubmit={event => {
             event.preventDefault();
 
             this.formatInput();
           }}
         >
-          <button>Submit</button>
-          <select
-            value={this.state.primarySelectBox}
-            onChange={this.changePrimarySelectBox}
-          >
-            <option value="">Select search term</option>
-            <option value="graduated">Graduated</option>
-            <option value="block">Block</option>
-          </select>
-          {this.state.primarySelectBox === "block" ? (
-            <label>
-              <select
-                value={this.state.secondarySelectBox}
-                onChange={this.changeSecondarySelectBox}
-              >
-                <option value="">Select search term</option>
-                <option value="grad">Grad</option>
-                <option value="fun">Fundamentals</option>
-                <option value="fe">Front end</option>
-                <option value="be">Back end</option>
-                <option value="proj">Projects</option>
-              </select>
-            </label>
-          ) : null}
-          {this.state.primarySelectBox === "graduated" ? (
-            <label>
-              <select
-                value={this.state.secondarySelectBox}
-                onChange={this.changeSecondarySelectBox}
-              >
-                <option value="">Select search term</option>
-                <option value="true">True</option>
-                <option value="false">false</option>
-              </select>
-            </label>
-          ) : null}
+          {" "}
+          <div className="searchTermGrid">
+            <button>Build Query</button>
+
+            <select
+              value={this.state.primarySelectBox}
+              onChange={this.changePrimarySelectBox}
+            >
+              <option value="">Select search term</option>
+              <option value="graduated">Graduated</option>
+              <option value="block">Block</option>
+              <option value="cohort">Cohort</option>
+            </select>
+            <div>
+              {this.state.primarySelectBox === "block" ? (
+                <label>
+                  <select
+                    required
+                    value={this.state.secondarySelectBox}
+                    onChange={this.changeSecondarySelectBox}
+                  >
+                    <option value="">Select search term</option>
+                    <option value="grad">Grad</option>
+                    <option value="fun">Fundamentals</option>
+                    <option value="fe">Front end</option>
+                    <option value="be">Back end</option>
+                    <option value="proj">Projects</option>
+                  </select>
+                </label>
+              ) : null}
+              {this.state.primarySelectBox === "graduated" ? (
+                <label>
+                  <select
+                    required
+                    value={this.state.secondarySelectBox}
+                    onChange={this.changeSecondarySelectBox}
+                  >
+                    <option disabled defaultValue>
+                      Select search term
+                    </option>
+                    <option value="true">True</option>
+                    <option value="false">false</option>
+                  </select>
+                </label>
+              ) : null}
+
+              {this.state.primarySelectBox === "cohort" ? (
+                <label>
+                  <input
+                    onChange={this.changeSecondarySelectBox}
+                    type="number"
+                    min="-1"
+                  ></input>
+                </label>
+              ) : null}
+            </div>
+          </div>
+          <p id="displaySearchTerms">{this.state.finalQuery}</p>
         </form>
+
         <button
           onClick={event => {
             this.props.resetStudents();
           }}
         >
           Reset
+        </button>
+        <button
+          onClick={() => {
+            console.log(this.state.finalQuery);
+            this.props.updateQuery(this.state.finalQuery);
+            this.setState({
+              input: "",
+              finalQuery: "",
+              primarySelectBox: "",
+              secondarySelectBox: ""
+            });
+          }}
+        >
+          Submit
         </button>
       </div>
     );
